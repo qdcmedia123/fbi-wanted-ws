7ff4a28b6f6054cf36d2637d60c41a9c9f7e69c2
bharat kumar shah <akashche72@gmail.com>
2021-03-20 16:57:36 +0400

    integration test is moved to old folder outside of src folder

--- fbi/__tests__/Integration.test.ts
+++ fbi/__tests__/Integration.test.ts
@@ -1,8 +1,57 @@
 /**
  * Test code goes here...
  */
-describe('Integration tests', () => {
-  test('Asynchronous test 1', async () => {
-    // Has written in routes folder 
-  })
-})
+
+import request from "supertest";
+import { app } from "../src/app";
+import { natsWrapper } from "../src/nats-wrapper";
+
+it("Throw en 401 auth error, if user is not authenticated", async () => {
+  await request(app).get("/api/fbi/list/sdfsdf").send({}).expect(401);
+});
+
+it("Throw an bad request error if pagination value set to string", async () => {
+  await request(app)
+    .get("/api/fbi/list/sdfsdf")
+    .set("Cookie", global.signin())
+    .expect(400);
+});
+
+it("Fetch fbi wanted list from remote api", async () => {
+  await request(app)
+    .get("/api/fbi/list")
+    .set("Cookie", global.signin())
+    .send()
+    .expect(200);
+});
+
+it("Fetch the FBI Want list api and publish the event", async () => {
+  const response = await request(app)
+    .get("/api/fbi/list")
+    .set("Cookie", global.signin())
+    .send()
+    .expect(200);
+  const parseString = JSON.parse(response.text);
+  expect(parseString.total).toBeDefined();
+  expect(parseString.items).toBeDefined();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('Throw 400 error if wanted list id is not found', async() => {
+   await request(app)
+  .get("/api/fbi/fbiById/testid")
+  .set("Cookie", global.signin())
+  .send()
+  .expect(400);
+});
+
+it('Fetch the FBI Wanted list individual item list if id is valid', async() => {
+  const response = await request(app)
+  .get("/api/fbi/fbiById/2978c2dee66b4a10a4b3030a00aa7db7")
+  .set("Cookie", global.signin())
+  .send()
+  .expect(200);
+  const parseString = JSON.parse(response.text);
+  expect(parseString.url).toBeDefined();
+});
+
